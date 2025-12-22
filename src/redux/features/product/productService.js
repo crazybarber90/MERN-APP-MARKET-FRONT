15cb2b4cff44b1af1d06f1915bc456bbe4515389
Nikola <pepy90aa@gmail.com>
2025-12-22 16:52:45 +0100

    removed slash

--- src/redux/features/product/productService.js
+++ src/redux/features/product/productService.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 // const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+
+// URL FROM public/config  & index.html
 const BACKEND_URL = window._env_.REACT_APP_BACKEND_URL
 const API_URL = `${BACKEND_URL}/api/products/`
 
