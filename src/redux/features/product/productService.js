cb4b73a8045e6fc0a7a1f234252a254a328ee995
Nikola <pepy90aa@gmail.com>
2025-12-22 15:40:26 +0100

    refactored productService - added withCredentials

--- src/redux/features/product/productService.js
+++ src/redux/features/product/productService.js
@@ -1,38 +1,43 @@
-import axios from "axios";
+import axios from 'axios'
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API_URL = `${BACKEND_URL}/api/products/`;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+const API_URL = `${BACKEND_URL}/api/products/`
+
+axios.defaults.withCredentials = true
 
 // CREATE NEW PRODUCT
 // WE DONT USE TRY / CATCH COS WE WILL USE HERE ASYNCTHUNK THIS TIME
 const createProduct = async (formData) => {
-  const response = await axios.post(API_URL, formData);
-  return response.data;
-};
+  const response = await axios.post(API_URL, formData)
+  return response.data
+}
 
 // GET ALL PRODUCTS
 const getProducts = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
-};
+  const response = await axios.get(API_URL)
+  return response.data
+}
 
 // DELETE PRODUCTS
 const deleteProduct = async (id) => {
-  const response = await axios.delete(API_URL + id);
-  return response.data;
-};
+  const response = await axios.delete(API_URL + id)
+  return response.data
+}
 
 // GET A PRODUCT
 const getProduct = async (id) => {
-  const response = await axios.get(API_URL + id);
-  return response.data;
-};
+  const response = await axios.get(API_URL + id)
+  if (!Array.isArray(response.data)) {
+    return []
+  }
+  return response.data
+}
 
 // UPDATE A PRODUCT
 const updateProduct = async (id, formData) => {
-  const response = await axios.patch(`${API_URL}${id}`, formData);
-  return response.data;
-};
+  const response = await axios.patch(`${API_URL}${id}`, formData)
+  return response.data
+}
 
 const productService = {
   createProduct,
@@ -40,6 +45,6 @@ const productService = {
   deleteProduct,
   getProduct,
   updateProduct,
-};
+}
 
-export default productService;
+export default productService
