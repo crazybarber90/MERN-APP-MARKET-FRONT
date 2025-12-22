982f37f859099b2208795154c20693cc85bb8d31
Nikola <pepy90aa@gmail.com>
2025-12-22 16:34:36 +0100

    added window.env

---
+++ public/config.js
@@ -0,0 +1,8 @@
+window._env_ = {
+  REACT_APP_BACKEND_URL:
+    'https://pinvent-api-bvcph8f7azakeebw.westeurope-01.azurewebsites-01.azurewebsites.net',
+}
+
+// window._env_ = {
+//   REACT_APP_BACKEND_URL: 'http://localhost:5000',
+// }
--- src/redux/features/product/productService.js
+++ src/redux/features/product/productService.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+// const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+const BACKEND_URL = window._env_.REACT_APP_BACKEND_URL
 const API_URL = `${BACKEND_URL}/api/products/`
 
 axios.defaults.withCredentials = true
